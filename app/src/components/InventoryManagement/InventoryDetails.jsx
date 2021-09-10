8ca6887524ea4ed6f97b07cace303f56ec9d5c9d
Mohomed Rikas <62861736+RikasMRM@users.noreply.github.com>
2021-09-10 18:55:34 +0530

    all inventory page

--- app/src/components/InventoryManagement/InventoryDetails.jsx
+++ app/src/components/InventoryManagement/InventoryDetails.jsx
@@ -1,21 +1,11 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
 
-
-class InventoryDetails extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+const InventoryDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
-
-InventoryDetails.propTypes = {
-
-};
-
-
-export default InventoryDetails;
+export default InventoryDetails
